742793aca65f9b85263f3bfabe81a096d2417141
tiago73 <tiagodsf73@gmail.com>
2020-04-24 04:08:12 -0300

    primeiro commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {Title} from "./styles"
+const Dashboard: React.FC = () => {
+    return <Title>Explore repository</Title>;
+}
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Repository: React.FC = () => {
+    return <h1>Repository</h1>;
+}
+export default Repository;
